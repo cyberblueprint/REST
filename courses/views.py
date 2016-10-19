183f605f7169aec5c40093b188f0681f9fe773f0
Rodrigo <email@gmail.com>
2016-10-19 16:32:36 -0300

    Overriding generic methods

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import generics
 
 from . import models
@@ -12,3 +14,29 @@ class ListCreateCourse(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+
+
+class ListCreateReview(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+    
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+    
+    # Prevents a user from being able to give a different pk when submiting
+    def perform_create(self, serializer):
+        course = get_object_or_404(
+            models.Course, pk=self.kwargs.get('course_pk'))
+        serializer.save(course=course)
+
+
+class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+    
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            course_id=self.kwargs.get('course_pk'),
+            pk=self.kwargs.get('pk')
+        )
